61b5c507a4578ef17441e7271aeb1678b16994eb
Frank-Grijalba <fjgrijalba@gmail.com>
2023-03-23 10:58:52 -0500

    Nuevo componente fjcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FjcodeComponent} from './pages/fjcode/fjcode.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FjcodeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/fjcode/fjcode.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector:'app-fjcode',
+    templateUrl:'./fjcode.component.html',
+    styleUrls:['./fjcode.component.scss']
+})
+
+export class FjcodeComponent implements OnInit{
+
+    constructor() {}
+    ngOnInit(): void {}
+}
